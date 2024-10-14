0fbd1ef210885d0ed5dbcc501209f29fedd9a62e
matias mavel <matias.mavel@saintmichelannecy.fr>
2024-10-14 14:04:15 +0200

    V2 finish

--- Hangman/MainWindow.xaml.cs
+++ Hangman/MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using System.Media;
 
 namespace Hangman
 {
@@ -27,27 +29,36 @@ namespace Hangman
             InitializeComponent();
         }
 
-        string[] listmot = { "vache", "bache", "chat", "cheval", "ordinateur" };
+
+        string[] listmot = File.ReadAllLines("../../mots.txt").ToArray();
         Random rnd = new Random();
         string motChoisi;  // Mot sélectionné
         char[] motAffiche;  // Mot avec les lettres devinées et les underscores
-        int vie = 5;  // Nombre de vies restantes
-
+        int vie = 7;  // Nombre de vies restantes
+        private SoundPlayer player = new SoundPlayer();
         // Lors du chargement de la fenêtre
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             StartNewGame();  // Commencer une nouvelle partie
         }
 
+        
+
         // Fonction pour démarrer une nouvelle partie
         private void StartNewGame()
         {
             motChoisi = listmot[rnd.Next(listmot.Length)];
             motAffiche = new string('_', motChoisi.Length).ToCharArray();  // Créer un tableau d'underscores pour le mot affiché
-            vie = 5;  // Réinitialiser les vies
+            vie = 7;  // Réinitialiser les vies
             UpdateMotAffiche();  // Mettre à jour l'affichage du mot
             EnableAllButtons();  // Activer tous les boutons
+            this.player.SoundLocation = @"../../sounds/music/music.wav";
+            this.player.PlayLooping();
+
+            
+
         }
+
 
         // Fonction appelée lors du clic sur un bouton de lettre
         private void BTN_click(object sender, RoutedEventArgs e)
@@ -62,7 +73,7 @@ namespace Hangman
                 for (int i = 0; i < motChoisi.Length; i++)
                 {
                     if (motChoisi[i].ToString() == letter)
-                    {
+                    {   
                         motAffiche[i] = motChoisi[i];  // Remplacer l'underscore par la lettre correcte
                     }
                 }
@@ -70,6 +81,8 @@ namespace Hangman
             else
             {
                 vie--;  // Réduire le nombre de vies
+                Uri pictures = new Uri("pictures/img/" + vie + ".png", UriKind.Relative);
+                Img_Hangman.Source = new BitmapImage(pictures);
                 if (vie == 0)
                 {
                     MessageBox.Show($"Perdu ! Le mot était : {motChoisi}");
@@ -105,6 +118,31 @@ namespace Hangman
                 }
             }
         }
+
+        private void Textboxw_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter) // Vérifie si la touche pressée est "Entrée"
+            {
+                // Vérifie si le texte n'est pas vide
+                if (!string.IsNullOrWhiteSpace(TextBoxw.Text))
+                {
+                    // Écrire le texte dans le fichier mots.txt
+                    using (var writer = new StreamWriter("../../mots.txt", true))
+                    {
+                        writer.WriteLine(TextBoxw.Text); // Ajouter le texte à la fin du fichier
+                    }
+                    MessageBox.Show("Texte ajouté au fichier !"); // Confirmation à l'utilisateur
+
+                    TextBoxw.Clear(); // Efface le contenu du TextBox après l'enregistrement
+                }
+                else
+                {
+                    MessageBox.Show("Veuillez entrer du texte avant d'appuyer sur Entrée !");
+                }
+            }
+        }
+
+
 
 
     }
